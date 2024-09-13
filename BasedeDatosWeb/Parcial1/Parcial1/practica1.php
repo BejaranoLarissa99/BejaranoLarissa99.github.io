e385cb39243b2100e37b007a7db828a37b7c0309
LarissaBejarano99 <159593456+BejaranoLarissa99@users.noreply.github.com>
2024-09-12 18:42:11 -0700

    Add files via upload

--- BasedeDatosWeb/Parcial1/Parcial1/practica1.php
+++ BasedeDatosWeb/Parcial1/Parcial1/practica1.php
@@ -2,8 +2,9 @@
 
     $nombre = $_GET["nombre"];
     $edad = $_GET["edad"];
+    $fecha_de_nacimeinto = $_GET["fecha_de_nacimiento"];
     $escuela = $_GET["escuela"];
-    $estado_civil = $_GET["estado civil"];
+    $estado_civil = $_GET["estado_civil"];
 
     if($edad > 18){
         echo "<h1>".$nombre." es mayor de edad</h1>";
@@ -14,6 +15,8 @@
     echo "<h1>Nombre del Alumno: ".$nombre."</h1>";
 
     echo "<h2>Edad: ".$edad."</h2>";
+
+    echo "<h2>Fecha de nacimiento: ".$fecha_de_nacimeinto."</h2>";
 
     echo "<h2>Escuela: ".$escuela."</h2>";
 
